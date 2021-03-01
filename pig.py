5bf28bdc21284177df2ab2d6619a6d7e66a6ef08
Marcelo A <marcelo_2012augusto@hotmail.com>
2021-03-01 18:12:15 -0300

    Preset with data.json file

--- pig.py
+++ pig.py
@@ -1,3 +1,4 @@
+# %%
 from pynput import mouse, keyboard
 from time import perf_counter, sleep
 from dataclasses import dataclass
@@ -5,6 +6,7 @@ from dataclasses import dataclass
 
 def print(x):
     import subprocess as s
+
     s.call(["notify-send", "--expire-time=1", x])
 
 
@@ -12,6 +14,7 @@ def print(x):
 class Command:
     time: float
     method: callable
+    args: list
 
 
 commands = []
@@ -23,35 +26,66 @@ class StopRecording(Exception):
     pass
 
 
+def kpress(key):
+    if isinstance(key, str):
+        k.press(key)
+    elif isinstance(key, int):
+        k.press(keyboard.KeyCode(key))
+
+
+def krelease(key):
+    if isinstance(key, str):
+        k.release(key)
+    elif isinstance(key, int):
+        k.release(keyboard.KeyCode(key))
+
+
 def on_press(key):
+    try:
+        ke = key.char
+    except AttributeError:
+        ke = key.value.vk
     if key == keyboard.Key.esc:
         raise StopRecording()
-    commands.append(Command(perf_counter(), lambda: k.press(key)))
+    commands.append(Command(perf_counter(), kpress, [ke]))
 
 
 def on_release(key):
-    commands.append(Command(perf_counter(), lambda: k.release(key)))
+    try:
+        ke = key.char
+    except AttributeError:
+        ke = key.value.vk
+    commands.append(Command(perf_counter(), krelease, [ke]))
+
+
+def move(x, y):
+    m.position = (x, y)
 
 
 def on_move(x, y):
-    def move():
-        m.position = (x, y)
+    commands.append(Command(perf_counter(), move, [x, y]))
 
-    commands.append(Command(perf_counter(), move))
+
+def cpress(button):
+    m.press(mouse.Button(button))
+
+
+def crelease(button):
+    m.release(mouse.Button(button))
 
 
 def on_click(x, y, button, pressed):
     on_move(x, y)
 
     if pressed:
-        commands.append(Command(perf_counter(), lambda: m.press(button)))
+        commands.append(Command(perf_counter(), cpress, [button.value]))
     if not pressed:
-        commands.append(Command(perf_counter(), lambda: m.release(button)))
+        commands.append(Command(perf_counter(), crelease, [button.value]))
 
 
 def on_scroll(x, y, dx, dy):
     on_move(x, y)
-    commands.append(Command(perf_counter(), lambda: m.scroll(dx, dy)))
+    commands.append(Command(perf_counter(), m.scroll, [dx, dy]))
 
 
 def wait_ready():
@@ -94,13 +128,83 @@ def play(x: int = 1, ignore_time=False):
             if previus_time:
                 sleep((c.time - previus_time) * (0.01 if ignore_time else 1))
             previus_time = c.time
-            c.method()
+            c.method(*c.args)
     print("Finished Playing...")
 
 
-if __name__ == "__main__":
+def validate():
     import sys
-    record()
+    import json
+    import pathlib
+    import os
+
+    f = pathlib.Path(__file__).parent.absolute() / "data.json"
+    mt = {
+        "kpress": kpress,
+        "krelease": krelease,
+        "move": move,
+        "cpress": cpress,
+        "crelease": crelease,
+        "scroll": m.scroll,
+    }
+
     x = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-    ig_t = True if len(sys.argv) > 2 else False
+    if len(sys.argv) > 2 and sys.argv[2] in ("True", "true", "t"):
+        ig_t = True
+    elif len(sys.argv) > 2 and sys.argv[2] in ("False", "false", "f"):
+        ig_t = False
+    else:
+        raise Exception("Invalid arg. Must be: pig 10[int] True[bool] --load=...[str] || --save=...[str]")
+
+    if len(sys.argv) > 3 and "--load=" in sys.argv[3]:
+        with open(f, "r") as file:
+            data = json.load(file)
+        for i, preset in enumerate(data):
+            if preset.get("name") == sys.argv[3].replace("--load=", ""):
+                commands.clear()
+                commands.extend([
+                    Command(c["time"], mt[c["method_name"]], c["method_args"])
+                    for c in preset["commands"]
+                ])
+                play(x, ig_t)
+                return
+
+    if not len(commands):
+        record()
+
+    if len(sys.argv) > 3 and "--save=" in sys.argv[3]:
+        if not os.path.exists(f):
+            with open(f, "w") as file:
+                json.dump([{"name": "example", "commands": []}], file)
+
+        with open(f, "r") as file:
+            data = json.load(file)
+
+        name = sys.argv[3].replace("--save=", "")
+        nd = {
+            "name": name,
+            "commands": [
+                {
+                    "time": c.time,
+                    "method_name": c.method.__name__,
+                    "method_args": c.args,
+                }
+                for c in commands
+            ],
+        }
+        for i, preset in enumerate(data):
+            if preset.get("name") == name:
+                data.pop(i)
+                data.append(nd)
+                break
+        else:
+            data.append(nd)
+
+        with open(f, "w") as file:
+            json.dump(data, file)
+
     play(x, ig_t)
+
+
+if __name__ == "__main__":
+    validate()
